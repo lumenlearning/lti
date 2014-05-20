fdeac6ccb7a88f4d704f5c2c6b8b352571fcd38b
Jeff Graham <jeff@funnymonkey.com>
2014-05-20 16:12:05 -0700

    Improve details on lti consumer view.

--- single-lti_consumer.php
+++ single-lti_consumer.php
@@ -12,16 +12,30 @@ get_header(); ?>
 	      // Start the Loop.
 	      while ( have_posts() ) {
 		      the_post();
-		      if ( get_post_meta( get_the_ID(), LTI_META_KEY_NAME, true ) ) {
-			      echo '<h2>Key: ';
-			      echo get_post_meta( get_the_ID(), LTI_META_KEY_NAME, true );
-			      echo '</h2>';
+          global $wpdb;
+
+          $endpoint = get_site_url(1) . '/api/lti/' . $wpdb->blogid;
+          echo '<div><label for="lti_consumer_endpoint">';
+          _e( 'Endpoint' );
+          echo ': </label>';
+          echo '<strong id="lti_consumer_endpoint" name="lti_consumer_endpoint">' . $endpoint . '</strong>';
+          echo '</div>';
+
+		      if ( $key = get_post_meta( get_the_ID(), LTI_META_KEY_NAME, true ) ) {
+            echo '<div><label for="lti_consumer_key">';
+            _e( 'Key' );
+            echo ': </label>';
+            echo '<strong id="lti_consumer_key" name="lti_consumer_key">' . esc_attr( $key ) . '</strong>';
+            echo '</div>';
 		      }
 
-		      if ( get_post_meta( get_the_ID(), LTI_META_SECRET_NAME, true ) ) {
-			      echo '<h2>Secret: ';
-			      echo get_post_meta( get_the_ID(), LTI_META_SECRET_NAME, true );
-			      echo '</h2>';
+
+		      if ( $secret = get_post_meta( get_the_ID(), LTI_META_SECRET_NAME, true ) ) {
+            echo '<div><label for="lti_consumer_secret">';
+            _e( 'Secret' );
+            echo ': </label>';
+            echo '<strong id="lti_consumer_secret" name="lti_consumer_secret">' . esc_attr( $secret ) . '</strong>';
+            echo '</div>';
 		      }
 	      }
       }
